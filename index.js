5d6c734a952bc3961aa3f01e20deb188e5b1b3f4
Matt Kuritz <mkuritz@thefarmersdog.com>
2020-07-21 22:12:24 -0400

    add error handling just in case

--- index.js
+++ index.js
@@ -19,28 +19,49 @@ let Foo = sequelize.define('foo', {
 });
 
 async function runTransactions() {
-  await Foo.sync({force: true});
-  await sequelize.transaction(async function () {
-    console.log('\n\ncreating bar');
-    await Foo.create({ unique: 'bar' });
-    console.log('\n\ncreating baz');
-    await Foo.create({ unique: 'baz' });
-    console.log('\n\nclosing transaction');
-    return;
-  });
-  await sequelize.transaction(async function () {
-    console.log('\n\nget bar')
-    let bar = await Foo.findOne({ where: {unique: 'bar'} });
-    console.log('bar id: ' + bar.id);
-    console.log('\n\nGets stuck on failed attempt to create bar a second time.');
-    await Foo.create({ unique: 'bar' });
-    /* Code below this point is never reached */
-    console.log('\n\nget baz');
-    let baz = await Foo.findOne({ where: {unique: 'baz'} });
-    console.log('baz id: ' + baz.id);
-    console.log('closing transaction');
-    return;
-  });
+  try {
+    await Foo.sync({force: true});
+    await sequelize.transaction(async function () {
+      try {
+        console.log('\n\ncreating bar');
+        await Foo.create({ unique: 'bar' });
+        console.log('\n\ncreating baz');
+        await Foo.create({ unique: 'baz' });
+        console.log('\n\nclosing transaction');
+      } catch (e) {
+        console.log('statement in transaction 1 failed')
+        throw e;
+      }
+    });
+  } catch (e) {
+    console.log('transaction 1 failed')
+    throw e;
+  }
+  try {
+    await sequelize.transaction(async function () {
+      try {
+        /* Uncomment error below for expected behavior */
+        //throw new Error;
+        console.log('\n\nget bar')
+        let bar = await Foo.findOne({ where: {unique: 'bar'} });
+        console.log('bar id: ' + bar.id);
+        console.log('\n\nGets stuck on failed attempt to create bar a second time.');
+        await Foo.create({ unique: 'bar' });
+        /* Code below this point is never reached */
+        console.log('\n\nget baz');
+        let baz = await Foo.findOne({ where: {unique: 'baz'} });
+        console.log('baz id: ' + baz.id);
+        console.log('closing transaction');
+      } catch (e) {
+        console.log('statement in transaction 2 failed')
+        throw e;
+      }
+    });
+  } catch (e) {
+    console.log('transaction 2 failed');
+    throw e;
+  }
+  console.log('end of run transactions');
 }
 
-runTransactions();
+runTransactions().then(() => console.log('script completed')).catch(() => console.log('caught err'));
